927298a06c8bb4fddd098f8cbfcc5b2c8135420d
Andrew Bromwich <a.bromwich@gmail.com>
2024-11-15 17:12:47 +1000

    Add info logging of the specs to be run (per runner)

--- lib/knapsack_pro/runners/rspec_runner.rb
+++ lib/knapsack_pro/runners/rspec_runner.rb
@@ -5,8 +5,10 @@ module KnapsackPro
         ENV['KNAPSACK_PRO_TEST_SUITE_TOKEN'] = KnapsackPro::Config::Env.test_suite_token_rspec
 
         runner = new(KnapsackPro::Adapters::RSpecAdapter)
+        test_file_paths = runner.stringify_test_file_paths
+        KnapsackPro.logger.info "About to test the following specs:\n\n#{test_file_paths}\n\n"
 
-        cmd = %Q[KNAPSACK_PRO_RECORDING_ENABLED=true KNAPSACK_PRO_TEST_SUITE_TOKEN=#{ENV['KNAPSACK_PRO_TEST_SUITE_TOKEN']} bundle exec rspec #{args} --default-path #{runner.test_dir} -- #{runner.stringify_test_file_paths}]
+        cmd = %Q[KNAPSACK_PRO_RECORDING_ENABLED=true KNAPSACK_PRO_TEST_SUITE_TOKEN=#{ENV['KNAPSACK_PRO_TEST_SUITE_TOKEN']} bundle exec rspec #{args} --default-path #{runner.test_dir} -- #{test_file_paths}]
 
         Kernel.system(cmd)
         Kernel.exit($?.exitstatus) unless $?.exitstatus.nil? || $?.exitstatus.zero?
